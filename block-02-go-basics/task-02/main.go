6bc20c1db911aa97596863b616ee17e9c941a019
nurdauletbeisenbek7-web <nurdauletbeisenbek7@gmail.com>
2026-09-21 18:07:41 +0500

    2 task  commit

---
+++ block-02-go-basics/task-02/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	fmt.Scan(&num1, &num2)
+	fmt.Printf("%d + %d = %d\n", num1, num2, num1+num2)
+	fmt.Printf("%d - %d = %d\n", num1, num2, num1-num2)
+	fmt.Printf("%d * %d = %d\n", num1, num2, num1*num2)
+}
