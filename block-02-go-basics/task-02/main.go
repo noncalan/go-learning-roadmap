a45bb1fda27a13460b2924650f7f192a150e94ec
nurdauletbeisenbek7-web <nurdauletbeisenbek7@gmail.com>
2026-09-21 18:11:53 +0500

    2 taskfix  commit

--- block-02-go-basics/task-02/main.go
+++ block-02-go-basics/task-02/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var num1, num2 int
 	fmt.Scan(&num1, &num2)
-	fmt.Printf("%d + %d = %d\n", num1, num2, num1+num2)
-	fmt.Printf("%d - %d = %d\n", num1, num2, num1-num2)
-	fmt.Printf("%d * %d = %d\n", num1, num2, num1*num2)
+	fmt.Println(num1 + num2)
+	fmt.Println(num1 - num2)
+	fmt.Println(num1 * num2)
 }
